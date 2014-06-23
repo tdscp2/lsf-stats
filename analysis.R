ef18d9e846c6e168715d34e408eee28b0c11d19a
tdscp2 <tdscp2@mail.missouri.edu>
2014-06-23 14:37:20 -0500

    Change

--- analysis.R
+++ analysis.R
@@ -1,11 +1,17 @@
  ## R analysis for lsf-stats Copyright 2014 Timothy Middelkoop and Tyler D. Stratton License Apache 2.0
  
  ## Read data
- d <- read.csv('lsf-stats/lewis-log.csv',sep="\t", header=TRUE);
+ setwd("C:/Users/tstratton/Documents/Github")
+ 
+ d <- read.csv('lsf-stats/lewis-log.csv',sep="\t", header=TRUE)
  n <- nrow(d) ; n
  names(d)
  
  summary(d$CPU_T)
  max(d$CPU_T)
- 
- 
+
+ onecore <- subset(d,d$cores==1)
+ summary(d$cores)
+normal <- subset(d, d$Queue=="normal")
+ normal[0:100,]
+
